f15429b41c6a85b34d9d21c68e7312fb075242a9
Thiago <thiago.ramos208@gmail.com>
2022-08-27 10:18:24 -0300

    Inserção background e logo

--- screens/home.js
+++ screens/home.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import { StyleSheet, View, Text, } from 'react-native';
+import { ImageBackground,StyleSheet, View, Text, Image} from 'react-native';
+
 
 export default function Home() {
+
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <ImageBackground  
+        source={require('../assets/background/busca_local.png')} 
+        style={styles.image}>
+
+    <Image 
+        style={styles.logo}
+        source={require('../assets/logo.png')} />
+
+
+      </ImageBackground>
     </View>
+
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+  },
+  logo: {
+    resizeMode:'center',
+    width: 300,
+    height:130,
+    margin: 47
+  }
+});
